6f6763632b4d96b14800e32631973bac6ed3f0dd
akaporn-katip <akaporn.katip@gmail.com>
2024-02-12 02:53:03 +0700

    fix user permission

--- lab-1/composefile/postgres/init-user-db.sh
+++ lab-1/composefile/postgres/init-user-db.sh
@@ -10,12 +10,12 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
                 grant all on schema public to devops;
 
 	create database sonar;
-            grant all on database gitea to devops;
+            grant all on database sonar to devops;
             \\c sonar;
                 grant all on schema public to devops;
 
 	create database grafana;
-            grant all on database gitea to devops;
+            grant all on database grafana to devops;
             \\c grafana;
                 grant all on schema public to devops;
 
